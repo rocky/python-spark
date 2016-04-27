0473a29cb34421c302ce5a36f09f6b684f099da6
rocky <rb@dustyfeet.com>
2016-04-27 02:37:00 -0400

    Export GenericASTBuilder

--- spark/__init__.py
+++ spark/__init__.py
@@ -3,11 +3,13 @@ PYTHON3 = (sys.version_info >= (3, 0))
 
 if PYTHON3:
     from spark.spark import GenericParser as GenericParser
+    from spark.spark import GenericASTBuilder as GenericASTBuiler
     from spark.spark import GenericASTTraversal as GenericASTTraversal
     from spark.ast import AST as AST
     from spark.scanner import GenericScanner as GenericScanner
     from spark.scanner import GenericToken as GenericToken
 else:
     from spark import GenericParser as GenericParser
+    from spark import GenericASTBuilder as GenericASTBuiler
     from spark import GenericASTTraversal as GenericASTTraversal
     from ast import AST as AST
