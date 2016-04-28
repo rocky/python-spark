3b5fc971c52d0a705f5634b5e99da6c55e8ce595
rocky <rb@dustyfeet.com>
2016-04-28 18:58:29 -0400

    Fix import

--- spark_parser/__init__.py
+++ spark_parser/__init__.py
@@ -12,10 +12,10 @@ if PYTHON3:
     from spark_parser.scanner import GenericToken as GenericToken
 else:
     from ast import AST as AST
-    from spark_parser import DEFAULT_DEBUG
-    from spark_parser import GenericParser as GenericParser
-    from spark_parser import GenericASTBuilder as GenericASTBuilder
-    from spark_parser import GenericASTTraversalPruningException as GenericASTTraversalPruningException
-    from spark_parser import GenericASTTraversal as GenericASTTraversal
+    from spark import DEFAULT_DEBUG
+    from spark import GenericParser as GenericParser
+    from spark import GenericASTBuilder as GenericASTBuilder
+    from spark import GenericASTTraversalPruningException as GenericASTTraversalPruningException
+    from spark import GenericASTTraversal as GenericASTTraversal
     from scanner import GenericScanner as GenericScanner
     from scanner import GenericToken as GenericToken
