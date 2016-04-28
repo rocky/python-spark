966d9386da1ed7e2978f0d7c1bb5249190d593da
rocky <rb@dustyfeet.com>
2016-04-27 23:16:50 -0400

    Remove __pkginfo__.py. Try pypy and pypy3

--- setup.py
+++ setup.py
@@ -2,18 +2,63 @@
 
 """Setup script for the 'uncompyle6' distribution."""
 
-# Get the package information used in setup().
-# from __pkginfo__ import \
-#     author,           author_email,       classifiers,                    \
-#     install_requires, license,            long_description,               \
-#     modname,          packages,           py_modules,   \
-#     short_desc,       version,            web,              zip_safe
+# To the extent possible we make this file look more like a
+# configuration file rather than code like setup.py. I find putting
+# configuration stuff in the middle of a function call in setup.py,
+# which for example requires commas in between parameters, is a little
+# less elegant than having it here with reduced code, albeit there
+# still is some room for improvement.
 
-from __pkginfo__ import \
-    author,           maintainer,      maintainer_email,             \
-    long_description,                                                \
-    modname,          py_modules,       					         \
-    short_desc,       version,          web,              zip_safe
+# Things that change more often go here.
+copyright   = """
+Copyright (C) 2016 Rocky Bernstein <rb@dustyfeet.com>.
+"""
+
+classifiers =  ['Development Status :: 3 - Alpha',
+                'Intended Audience :: Developers',
+                'Operating System :: OS Independent',
+                'Programming Language :: Python',
+                'Programming Language :: Python :: 2',
+                'Programming Language :: Python :: 2.6',
+                'Programming Language :: Python :: 2.7',
+                'Programming Language :: Python :: 3',
+                'Programming Language :: Python :: 3.3',
+                'Programming Language :: Python :: 3.4',
+                'Programming Language :: Python :: 3.5',
+                'Topic :: Software Development :: Code Generators'
+                'Topic :: Software Development :: Compilers '
+                'Topic :: Software Development :: Libraries :: Python Modules',
+                ]
+
+# The rest in alphabetic order
+author             = "John Aycock"
+ftp_url            = None
+# license            = 'BSDish'
+maintainer         = "Rocky Bernstein"
+maintainer_email   = "rb@dustyfeet.com"
+modname            = 'spark'
+py_modules         = None
+short_desc         = 'An Early-Algorithm LR Parser'
+
+import os.path
+
+
+def get_srcdir():
+    filename = os.path.normcase(os.path.dirname(os.path.abspath(__file__)))
+    return os.path.realpath(filename)
+
+ns = {}
+version            = '1.0.2'
+web                = 'https://github.com/rocky/python-spark/'
+
+# tracebacks in zip files are funky and not debuggable
+zip_safe = True
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+long_description   = ( read("README.rst") + '\n' )
 
 __import__('pkg_resources')
 from setuptools import setup
