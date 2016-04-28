ef9b98b36db77550e7dc893d396482846dd100ba
rocky <rb@dustyfeet.com>
2016-04-27 22:53:29 -0400

    Fix module exports and update NEWS

--- spark/__init__.py
+++ spark/__init__.py
@@ -2,14 +2,20 @@ import sys
 PYTHON3 = (sys.version_info >= (3, 0))
 
 if PYTHON3:
-    from spark.spark import GenericParser as GenericParser
-    from spark.spark import GenericASTBuilder as GenericASTBuiler
-    from spark.spark import GenericASTTraversal as GenericASTTraversal
     from spark.ast import AST as AST
+    from spark.spark import DEFAULT_DEBUG
+    from spark.spark import GenericParser as GenericParser
+    from spark.spark import GenericASTBuilder as GenericASTBuilder
+    from spark.spark import GenericASTTraversalPruningException as GenericASTTraversalPruningException
+    from spark.spark import GenericASTTraversal as GenericASTTraversal
     from spark.scanner import GenericScanner as GenericScanner
     from spark.scanner import GenericToken as GenericToken
 else:
-    from spark import GenericParser as GenericParser
-    from spark import GenericASTBuilder as GenericASTBuiler
-    from spark import GenericASTTraversal as GenericASTTraversal
     from ast import AST as AST
+    from spark import DEFAULT_DEBUG
+    from spark import GenericParser as GenericParser
+    from spark import GenericASTBuilder as GenericASTBuilder
+    from spark import GenericASTTraversalPruningException as GenericASTTraversalPruningException
+    from spark import GenericASTTraversal as GenericASTTraversal
+    from scanner import GenericScanner as GenericScanner
+    from scanner import GenericToken as GenericToken
