eb66726d09bedc054dbab9271f5d67546828e581
rocky <rb@dustyfeet.com>
2016-06-10 19:07:04 -0400

    Move AST tree routines from parser to ast.py

--- spark_parser/__init__.py
+++ spark_parser/__init__.py
@@ -8,19 +8,19 @@ PYTHON3 = (sys.version_info >= (3, 0))
 
 if PYTHON3:
     from spark_parser.ast import AST as AST
+    from spark_parser.ast import GenericASTTraversal as GenericASTTraversal
+    from spark_parser.ast import GenericASTTraversalPruningException as GenericASTTraversalPruningException
     from spark_parser.spark import DEFAULT_DEBUG
     from spark_parser.spark import GenericParser as GenericParser
     from spark_parser.spark import GenericASTBuilder as GenericASTBuilder
-    from spark_parser.spark import GenericASTTraversalPruningException as GenericASTTraversalPruningException
-    from spark_parser.spark import GenericASTTraversal as GenericASTTraversal
     from spark_parser.scanner import GenericScanner as GenericScanner
     from spark_parser.scanner import GenericToken as GenericToken
 else:
     from ast import AST as AST
+    from ast import GenericASTTraversal as GenericASTTraversal
+    from ast import GenericASTTraversalPruningException as GenericASTTraversalPruningException
     from spark import DEFAULT_DEBUG
     from spark import GenericParser as GenericParser
     from spark import GenericASTBuilder as GenericASTBuilder
-    from spark import GenericASTTraversalPruningException as GenericASTTraversalPruningException
-    from spark import GenericASTTraversal as GenericASTTraversal
     from scanner import GenericScanner as GenericScanner
     from scanner import GenericToken as GenericToken
