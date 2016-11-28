8367fbb37dafa1717cd02135f8c92c6a9a69450c
rocky <rb@dustyfeet.com>
2016-11-28 02:52:29 -0500

    Update short description
    Add 'dups' in expr2/parser.py

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ maintainer_email   = "rb@dustyfeet.com"
 modname            = 'spark_parser'
 name               = 'spark_parser'
 py_modules         = None
-short_desc         = 'An Early-Algorithm Context-free grammar Parser'
+short_desc         = 'An Early-Algorithm Context-free grammar Parser Toolkit'
 web                = 'https://github.com/rocky/python-spark/'
 
 # tracebacks in zip files are funky and not debuggable
