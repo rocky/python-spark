32da74b107703d67718ddfe187142cae9232834d
rocky <rb@dustyfeet.com>
2017-01-29 23:46:41 -0500

    Add spark-parser-coverage CLI program

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 
 # Things that change more often go here.
 copyright = """
-Copyright (C) 2016, 2022, 2024 Rocky Bernstein <rb@dustyfeet.com>.
+Copyright (C) 2016-2017, 2022, 2024 Rocky Bernstein <rb@dustyfeet.com>.
 """
 
 from setuptools import setup
@@ -19,7 +19,6 @@ classifiers =  ['Development Status :: 4 - Beta',
                 'Intended Audience :: Developers',
                 'Operating System :: OS Independent',
                 'Programming Language :: Python',
-                'Programming Language :: Python :: 2.3',
                 'Programming Language :: Python :: 2.4',
                 'Programming Language :: Python :: 2.5',
                 'Programming Language :: Python :: 2.6',
@@ -27,8 +26,55 @@ classifiers =  ['Development Status :: 4 - Beta',
                 'Programming Language :: Python :: 3.3',
                 'Programming Language :: Python :: 3.4',
                 'Programming Language :: Python :: 3.5',
+                'Programming Language :: Python :: 3.6',
                 'Topic :: Software Development :: Code Generators',
                 'Topic :: Software Development :: Compilers ',
                 'Topic :: Software Development :: Libraries :: Python Modules',
                 ]
-setup()
+# The rest in alphabetic order
+author             = "John Aycock"
+ftp_url            = None
+license            = 'MIT'
+maintainer         = "Rocky Bernstein"
+maintainer_email   = "rb@dustyfeet.com"
+modname            = 'spark_parser'
+name               = 'spark_parser'
+py_modules         = None
+short_desc         = 'An Earley-Algorithm Context-free grammar Parser Toolkit'
+web                = 'https://github.com/rocky/python-spark/'
+
+# tracebacks in zip files are funky and not debuggable
+zip_safe = True
+
+import os
+def get_srcdir():
+    filename = os.path.normcase(os.path.dirname(os.path.abspath(__file__)))
+    return os.path.realpath(filename)
+
+srcdir = get_srcdir()
+
+def read(*rnames):
+    return open(os.path.join(srcdir, *rnames)).read()
+
+# Get info from files; set: long_description and VERSION
+long_description   = ( read("README.rst") + '\n' )
+exec(read('spark_parser/version.py'))
+
+from setuptools import setup, find_packages
+setup(
+       classifiers        = classifiers,
+       description        = short_desc,
+       # install_requires   = install_requires,
+       license            = license,
+       long_description   = long_description,
+       maintainer         = maintainer,
+       maintainer_email   = maintainer_email,
+       packages           = find_packages(),
+       py_modules         = py_modules,
+       name               = name,
+       scripts            = ['bin/spark-parser-coverage'],
+       test_suite         = 'nose.collector',
+       url                = web,
+       tests_require     = ['nose>=1.0'],
+       version            = VERSION,
+       zip_safe           = zip_safe)
