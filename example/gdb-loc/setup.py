6ccd99ed247eca0f39eb7038bd4be61f865c28c6
rocky <rb@dustyfeet.com>
2017-10-10 13:09:29 -0400

    Python 2.4 compatibility after master merge

---
+++ example/gdb-loc/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(
+       description        = "gdb location example",
+       install_requires   = ['spark_parser >= 1.7.0'],
+       name               = 'gdbloc',
+       version            = '1.0'
+)
