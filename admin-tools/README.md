0006c0f51549a01fde2075f48dd318e692cdf8db
rocky <rb@dustyfeet.com>
2017-10-12 20:38:28 -0400

    Add admin code for making a release

---
+++ admin-tools/README.md
@@ -0,0 +1,10 @@
+Making a release is a somewhat tedious process so I've automated it a little
+
+
+Here are tools that I, rocky, use to check and build a distribution.
+
+They are customized to my environment:
+- I use pyenv to various Python versions installed
+
+There may be other rocky-specific things that need customization.
+how-to-make-a-release.txt has overall how I make a release
---
+++ admin-tools/setup-master.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+PYTHON_VERSION=3.6.3
+
+# FIXME put some of the below in a common routine
+function finish {
+  cd $owd
+}
+
+export PATH=$HOME/.pyenv/bin/pyenv:$PATH
+owd=$(pwd)
+bs=${BASH_SOURCE[0]}
+if [[ $0 == $bs ]] ; then
+    echo "This script should be *sourced* rather than run directly through bash"
+    exit 1
+fi
+mydir=$(dirname $bs)
+fulldir=$(readlink -f $mydir)
+cd $fulldir/..
+git checkout master && pyenv local $PYTHON_VERSION
+cd $owd
