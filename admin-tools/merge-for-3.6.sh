20471504c7f1bcb76937caec488583f74c578f1f
rocky <rb@dustyfeet.com>
2024-10-03 18:37:25 -0400

    Start separting code into verison branches

---
+++ admin-tools/merge-for-3.6.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+# Setup for running Python 3.6 .. 3.10, merging master into this branch
+spark_36_owd=$(pwd)
+cd $(dirname ${BASH_SOURCE[0]})
+(cd .. && PYTHON_VERSION=3.6 && pyenv local $PYTHON_VERSION)
+if . ./setup-python-3.6.sh; then
+    git merge master
+fi
+cd $spark_36_owd
