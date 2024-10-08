a937aca481f7c0e235f3e7f2b288b79d4b879bff
rocky <rb@dustyfeet.com>
2024-10-08 08:08:49 -0400

    Go over make dist scripts

---
+++ admin-tools/make-dist-2.4-2.7.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+PACKAGE=spark_parser
+
+# FIXME put some of the below in a common routine
+function finish {
+  cd $make_spark_dist_24_owd
+}
+make_spark_dist_24_owd=$(pwd)
+trap finish EXIT
+
+cd $(dirname ${BASH_SOURCE[0]})
+if ! source ./pyenv-2.4-2.7-versions ; then
+    exit $?
+fi
+if ! source ./setup-python-2.4.sh ; then
+    exit $?
+fi
+
+cd ..
+source $PACKAGE/version.py
+if [[ ! -n $__version__ ]]; then
+    echo "You need to set __version__ first"
+    exit 1
+fi
+echo $__version__
+
+for pyversion in $PYVERSIONS; do
+    echo --- $pyversion ---
+    if [[ ${pyversion:0:4} == "pypy" ]] ; then
+	echo "$pyversion - PyPy does not get special packaging"
+	continue
+    fi
+    if ! pyenv local $pyversion ; then
+	exit $?
+    fi
+
+    rm -fr build
+    python setup.py bdist_egg
+    echo === $pyversion ===
+done
+
+echo "--- python 2.7 wheel ---"
+pyenv local 2.7.18
+python setup.py bdist_wheel
+echo === $pyversion ===
+
+# PyPI can only have one source tarball.
+# Tarballs can get created from the above setup, so make sure to remove them since we want
+# the tarball from master.
+
+tarball=dist/${PACKAGE}-$__version__-tar.gz
+if [[ -f $tarball ]]; then
+    rm -v dist/${PACKAGE}-$__version__-tar.gz
+fi
+finish
