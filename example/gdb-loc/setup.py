42951675e23d7ec4d83b47246f41be73b520929b
rocky <rb@dustyfeet.com>
2024-10-08 07:20:09 -0400

    Merge hell

--- example/gdb-loc/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-setup(
-       description        = "gdb location example",
-       install_requires   = ['spark_parser >= 1.7.0'],
-       name               = 'gdbloc',
-       version            = '1.0'
-)
