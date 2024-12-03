827a5b91abf0c66c3d0e06d7d4950034ca2c59fb
rocky <rb@dustyfeet.com>
2024-12-02 20:31:03 -0500

    Administrivia

--- admin-tools/setup-python-2.4.sh
+++ admin-tools/setup-python-2.4.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 PYTHON_VERSION=2.4.6
 
-owd=$(pwd)
+spark_owd=$(pwd)
 bs=${BASH_SOURCE[0]}
 if [[ $0 == $bs ]] ; then
     echo "This script should be *sourced* rather than run directly through bash"
     exit 1
 fi
 mydir=$(dirname $bs)
-fulldir=$(readlink -f $mydir)
-cd $fulldir/..
+cd $mydir/..
 git checkout python-2.4-to-2.7 &&  pyenv local $PYTHON_VERSION
-cd $owd
+cd $spark_owd
